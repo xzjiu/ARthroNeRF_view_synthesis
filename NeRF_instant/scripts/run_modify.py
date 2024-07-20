d12a7bdd86c0941ace481e5ca7113cf5cdadccec
ZZYROBO123 <141454379+ZZYROBO123@users.noreply.github.com>
2024-07-20 20:21:25 +0800

    Update run_modify.py

--- NeRF_instant/scripts/run_modify.py
+++ NeRF_instant/scripts/run_modify.py
@@ -29,11 +29,8 @@ import pyngp as ngp # noqa
 from sksurgerynditracker.nditracker import NDITracker
 import pickle
 from scipy.spatial.transform import Rotation as R
+# import keyboard
 
-import keyboard  # using module keyboard
-import csv
-
-import time
 
 def flip_matrix(m):
 	c2w = np.linalg.inv(m)
@@ -82,19 +79,6 @@ def rotation_matrix_y(angle_deg):
         [0, 1, 0],
         [-sin_theta, 0, cos_theta]
     ])
-
-def calculate_relative_positions(base_pos, stream):
-    relative_stream = []
-    for i, data in stream:
-        # Assuming the last column of both base_pos and data arrays hold the relevant 3D positions
-        base_column = base_pos[:3, -1]
-        data_column = data[:3, -1]
-        
-        # Subtract the two columns to get the relative position
-        relative_data = data_column - base_column
-        relative_stream.append([i, list(relative_data)])
-    return relative_stream
-
 def parse_args():
 	parser = argparse.ArgumentParser(description="Run instant neural graphics primitives with additional configuration & output options")
 
@@ -108,15 +92,12 @@ def parse_args():
 	parser.add_argument("--save_snapshot", default="", help="Save this snapshot after training. recommended extension: .ingp/.msgpack")
 
 	parser.add_argument("--nerf_compatibility", action="store_true", help="Matches parameters with original NeRF. Can cause slowness and worse results on some scenes, but helps with high PSNR on synthetic scenes.")
-	#################
 	parser.add_argument("--test_transforms", default="", help="Path to a nerf style transforms json from which we will compute PSNR.")
 	parser.add_argument("--near_distance", default=-1, type=float, help="Set the distance from the camera at which training rays start for nerf. <0 means use ngp default")
 	parser.add_argument("--exposure", default=0.0, type=float, help="Controls the brightness of the image. Positive numbers increase brightness, negative numbers decrease it.")
-    
-	##################
+
 	parser.add_argument("--screenshot_transforms", default="", help="Path to a nerf style transforms.json from which to save screenshots.")
 	parser.add_argument("--screenshot_frames", nargs="*", help="Which frame(s) to take screenshots of.")
-	##################
 	parser.add_argument("--screenshot_dir", default="", help="Which directory to output screenshots to.")
 	parser.add_argument("--screenshot_spp", type=int, default=16, help="Number of samples per pixel in screenshots.")
 
@@ -147,16 +128,12 @@ def parse_args():
 	parser.add_argument("--row", default="c:/Users/camp/Documents/Xinrui Zou/Tool-ROM/no-pivot.rom", help="used only when there is a ndi")
 	# parser.add_argument("--row", default="c:/Users/camp/Documents/Xinrui Zou/Tool-ROM/plexy_pointer.rom", help="used only when there is a ndi")
 	# parser.add_argument("--calibration", default="C:/Users/camp/GIT/arthro_nerf/camera_calibration/handeye_matrix_new_2.csv")
-	parser.add_argument("--calibration", default="C:/Users/camp/GIT/arthro_nerf/handeye-v2/handeye_matrix.csv")
-	parser.add_argument("--world_center", default="C:/Users/camp/GIT/arthro_nerf/Newplatformar/world_center.pkl")
+	parser.add_argument("--calibration", default="C:/Users/camp/GIT/arthro_nerf/new_calibration_5/handeye_matrix.csv")
+	parser.add_argument("--world_center", default="C:/Users/camp/GIT/arthro_nerf/NewPlatform1/world_center.pkl")
 	# parser.add_argument("--post_R", default="C:/Users/camp/GIT/arthro_nerf/LEGO/postprocess-R.pkl")
-	parser.add_argument("--post_info", default="C:/Users/camp/GIT/arthro_nerf/Newplatformar/postprocess-info.pkl")
-	parser.add_argument("--evaluation_list", default="C:/Users/camp/GIT/arthro_nerf/Newplatformar/evaluation.csv")
-
-	# Evaluation
-	parser.add_argument("--evaluation",action="store_true", help="Whether to do the evaluation with baseboard")
-	parser.add_argument("--base_pose",  default="C:/Users/camp/GIT/arthro_nerf/utils/evaluations/base_pose.pickle", help="used only when there is a ndi")
+	parser.add_argument("--post_info", default="C:/Users/camp/GIT/arthro_nerf/NewPlatform1/postprocess-info.pkl")
 	parser.add_argument("--user_id", type=float, default=0, help="get the user number")
+
 	return parser.parse_args()
 
 def get_scene(scene):
@@ -235,6 +212,7 @@ if __name__ == "__main__":
 		print("NeRF training ray near_distance ", args.near_distance)
 		testbed.nerf.training.near_distance = args.near_distance
 
+	args.user_id 
 	if args.nerf_compatibility:
 		print(f"NeRF compatibility mode enabled")
 
@@ -278,7 +256,6 @@ if __name__ == "__main__":
 	tqdm_last_update = 0
 	test_list = [[0,0,0],[0.2,0.2,0.2],[0.5,0.5,0.5]]
 	tool2camera = np.loadtxt(args.calibration, dtype=float, delimiter=',')
-	
 	camera2tool = np.linalg.inv(tool2camera)
 	with open(args.world_center, 'rb') as f:
 		world_center = pickle.load(f)
@@ -292,47 +269,25 @@ if __name__ == "__main__":
 
 	i = 0
 
-	base_pose = None
-	if args.evaluation:
-		with open(args.base_pose, 'rb') as handle:
-			base_pose = pickle.load(handle)
-		
-	testbed.usernum = args.user_id
-	# Define a list to store the relative position 
-	stream_list = []
-	stream = []
-	j = 0
-	last_pressed_time = 0
-	key_held = False
-	pressnum_initialized = False
-
-	prev_timer_value = 0
-	start_time = None
-	press_count = 0
-	times_recorded = []
-	time_duration = 0
-
+	toggle = False
 	if n_steps > 0:
 		with tqdm(desc="Training", total=n_steps, unit="steps") as t:
 			while testbed.frame():
-				if not pressnum_initialized:
-					testbed.pressnum = [0,0,0]
-					pressnum_initialized = True
 				# TODO: ndi controller
 				if args.gui and args.ndi:
 					port_handles, timestamps, framenumbers, tracking, quality = TRACKER.get_frame()
 
 					if not np.isnan(tracking[0][0][-1]):
 						transform_matrix = tracking[0]
-						# print("T1", transform_matrix)
+						print("T1", transform_matrix)
+
 
 						# ####### 1. handeye calibration
 						transform_matrix = camera2tool @ np.linalg.inv(transform_matrix)
-						# print("T2", transform_matrix)
+						print("T2", transform_matrix)
 						# ####### 2. use world center as origin ############
-						# print("tool2camera", tool2camera)
 						transform_matrix = transform_matrix @ world_center @ tool2camera
-						# print("T3", transform_matrix)
+						print("T3", transform_matrix)
 
 						# # 1,2 Get right matix direction.
 
@@ -346,165 +301,99 @@ if __name__ == "__main__":
 						# transform_matrix[0:3, 3] -= post_totp
                         # # # ####### 6.
 						avg_post_avglen = sum(post_avglen) / len(post_avglen)
-						# print("T4", transform_matrix)
+						print("T4", transform_matrix)
 						transform_matrix[0:3, 3] *= 4.0 / avg_post_avglen  # scale to "nerf sized"
 						#transform_matrix = transform_matrix.tolist()
 						# print(transform_matrix)
 						tran_vec = transform_matrix[[0,1,2],-1]
 						center = [0.5,-0.50478,0]
-
-						
 						
 						# if keyboard.is_pressed('k'):
-						
-
-
-						if args.evaluation:
-							# Detect a change in testbed.timer value
-							if testbed.timer != prev_timer_value:
-								if start_time is not None:  # Check if there's already a time being recorded
-									times_recorded.append(time.time() - start_time)  # Store the duration for the previous timer value
-								start_time = time.time()  # Start a new recording
-								prev_timer_value = testbed.timer  # Update the previous timer value
-
-							if keyboard.is_pressed('l'):
-								testbed.pressnum = [0,0,0]  # Reset pressnum to [0,0,0]
-								j = 0  # Reset any related counters
-								testbed.pressnum = [j,0,0]
-
-							if keyboard.is_pressed(' '):
-								if keyboard.is_pressed(' ') and (time.time() - last_pressed_time >= 0.5):
-									if not key_held:
-										x = np.array([
-										[1.79233051e-01, 7.81816542e-02, 9.80695234e-01, 2.69621530e+02],
-										[2.05724419e-01, -9.77777731e-01, 4.03506246e-02, -2.86812080e+00],
-										[9.62056639e-01, 1.94520791e-01, -1.91333961e-01, -6.97097727e+01],
-										[0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
-										])
-										transformed_data = tracking[0] @ x
-										stream = [[j, transformed_data]]
-										print('stream', stream)
-										
-										relative_stream = calculate_relative_positions(np.array(base_pose), stream)  # Converted base_pos to a NumPy array
-										print(f"Relative stream: {relative_stream}")
-										stream_list.append(relative_stream)
-										print('Updated stream_list:', stream_list)  # Print the updated stream_list
-										j = j+1
-										press_count += 1
-										# Check if space has been pressed 10 times
-										if press_count % 10 == 0:
-											if start_time is not None:
-												times_recorded.append(time.time() - start_time)  # Store the duration
-												start_time = None  # Reset start_time
-
-										testbed.pressnum = [j,0,0]
-										key_held = True
-										last_pressed_time = time.time()
-									else:
-										key_held = False
-						time_duration = 0 if start_time is None else time.time() - start_time
-						testbed.tasktime = time_duration
-						#print("time go: ", time_duration)
-						print("time: ", times_recorded)
-						testbed.look_at = [center[0] + tran_vec[1]/289.13, 0.5 + tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
-						# print("camera_look_at: ", testbed.camera_matrix)
-						# Extract translation
-						translation = transform_matrix[:3,3]
-					
-						# Set view rotation with ? axis and 30 degrees
-						rot_matrix_x = rotation_matrix_x(45)
-						rot_matrix_y = rotation_matrix_y(45)
-						rot_matrix_z = rotation_matrix_z(45)
-						# The rotation matrix part
-						rotation_matrix = transform_matrix[:3,:3]
-						# Change the view if observation
-						rotation_matrix = rotation_matrix @ rot_matrix_x
-						vector = rotation_matrix_to_vector(rotation_matrix)
-						testbed.view_dir = vector
-						# print("camera_view_dir: ", testbed.camera_matrix)
-
-						#Get up_dir
-						vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
-						testbed.up_dir = vector_up
-
-						testbed.scale = 1.025
-						k = testbed.camera_matrix
-						# print("Transfer: ", k)
-						k4 = k[:, -1]
-						k1 = k[:, 0]
-						k2 = k[:, 1]
-						k3 = k[:, 2]
-						testbed.ndi_rotation = k4
-						testbed.ndi_camera_1 = k1
-						testbed.ndi_camera_2 = k2
-						testbed.ndi_camera_3 = k3
-						# print("First Person View")
-						testbed.look_at = [center[0] + tran_vec[1]/289.13, 0.5 + tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
-						# print("camera_look_at1: ", testbed.camera_matrix)
-						# Extract translation
-						translation = transform_matrix[:3,3]
-						# The rotation matrix part
-						rotation_matrix = transform_matrix[:3,:3]
-						# print("R1", rotation_matrix)
-						vector = rotation_matrix_to_vector(rotation_matrix)
-						testbed.view_dir = vector
-						# print("camera_view_dir1: ", testbed.camera_matrix)
-						# print("Final: ", testbed.camera_matrix)
-						# print("Origin vector:", vector)
-						#Get up_dir
-						vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
-						testbed.up_dir = vector_up
-						testbed.scale = tran_vec[2]*0.0036754*0.4
-						testbed.reset_accumulation()
+						toggle = True
 							
-
-						# else:
-						# 		print("Third Person View")
-
-						# 		#Set offset
-						# 		testbed.look_at = [center[0] + tran_vec[1]/289.13, 0.5 + tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
-
-						# 		# Extract translation
-						# 		translation = transform_matrix[:3,3]
+						if toggle:
+								
+								# print("initial: ", testbed.camera_matrix)
+								testbed.look_at = [center[0] + tran_vec[1]/289.13, 0.5 + tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
+								# print("camera_look_at: ", testbed.camera_matrix)
+								# Extract translation
+								translation = transform_matrix[:3,3]
 					
-						# 		# Set view rotation with ? axis and 30 degrees
-						# 		rot_matrix_x = rotation_matrix_x(45)
-						# 		rot_matrix_y = rotation_matrix_y(45)
-						# 		rot_matrix_z = rotation_matrix_z(45)
-						# 		# The rotation matrix part
-						# 		rotation_matrix = transform_matrix[:3,:3]
-						# 		# Change the view if observation
-						# 		rotation_matrix = rotation_matrix @ rot_matrix_x
-						# 		vector = rotation_matrix_to_vector(rotation_matrix)
-						# 		testbed.view_dir = vector
+								# Set view rotation with ? axis and 30 degrees
+								rot_matrix_x = rotation_matrix_x(45)
+								rot_matrix_y = rotation_matrix_y(45)
+								rot_matrix_z = rotation_matrix_z(45)
+								# The rotation matrix part
+								rotation_matrix = transform_matrix[:3,:3]
+								# Change the view if observation
+								rotation_matrix = rotation_matrix @ rot_matrix_x
+								vector = rotation_matrix_to_vector(rotation_matrix)
+								testbed.view_dir = vector
+								# print("camera_view_dir: ", testbed.camera_matrix)
 
-						# 		#Get up_dir
-						# 		vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
-						# 		testbed.up_dir = vector_up
+								#Get up_dir
+								vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
+								testbed.up_dir = vector_up
 
-						# 		testbed.scale = 1.0
-						# 		print("third: ", testbed.camera_matrix)
-						# 		testbed.reset_accumulation()
+								testbed.scale = 1.0
+								k = testbed.camera_matrix
+								# print("Transfer: ", k)
+								k4 = k[:, -1]
+								k1 = k[:, 0]
+								k2 = k[:, 1]
+								k3 = k[:, 2]
+								testbed.ndi_rotation = k4
+								testbed.ndi_camera_1 = k1
+								testbed.ndi_camera_2 = k2
+								testbed.ndi_camera_3 = k3
+								# print("First Person View")
+								testbed.look_at = [center[0] + tran_vec[1]/289.13, center[1]+tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
+								# print("camera_look_at1: ", testbed.camera_matrix)
+								# Extract translation
+								translation = transform_matrix[:3,3]
+								# The rotation matrix part
+								rotation_matrix = transform_matrix[:3,:3]
+								# print("R1", rotation_matrix)
+								vector = rotation_matrix_to_vector(rotation_matrix)
+								testbed.view_dir = vector
+								# print("camera_view_dir1: ", testbed.camera_matrix)
+								# print("Final: ", testbed.camera_matrix)
+								# print("Origin vector:", vector)
+								#Get up_dir
+								vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
+								testbed.up_dir = vector_up
+								testbed.scale = 1.0
+								testbed.reset_accumulation()
 
-                        # TODO: Evaluation with baseboard position
 
-						
+						else:
+								print("Third Person View")
+
+								#Set offset
+								testbed.look_at = [center[0] + tran_vec[1]/289.13, 0.5 + tran_vec[2]*0.0036754 , 0.5 + tran_vec[0]/313.7255]
+
+								# Extract translation
+								translation = transform_matrix[:3,3]
+					
+								# Set view rotation with ? axis and 30 degrees
+								rot_matrix_x = rotation_matrix_x(45)
+								rot_matrix_y = rotation_matrix_y(45)
+								rot_matrix_z = rotation_matrix_z(45)
+								# The rotation matrix part
+								rotation_matrix = transform_matrix[:3,:3]
+								# Change the view if observation
+								rotation_matrix = rotation_matrix @ rot_matrix_x
+								vector = rotation_matrix_to_vector(rotation_matrix)
+								testbed.view_dir = vector
+
+								#Get up_dir
+								vector_up = [rotation_matrix[1, 1], rotation_matrix[2, 1],rotation_matrix[0, 1]]
+								testbed.up_dir = vector_up
+
+								testbed.scale = 1.0
+								print("third: ", testbed.camera_matrix)
+								testbed.reset_accumulation()
 							
-
-
-
-						#print("testbed.frame:", testbed.frame)
-						#print("testbed.first_training_view:", testbed.first_training_view)
-
-						
-						
-						
-
-
-						# TODO: add the following transforms in `world_extrinsic_mapping.py` or find a way to simplify the procedure
-						# TODO: change testbed.look_at/ testbed.view_dir/ testbed.scale and add constrain to the view
-						# TODO: compare the camera ground truth with the generated viewpoint
-
 				if testbed.want_repl():
 					repl(testbed)
 				# What will happen when training is done?
@@ -525,12 +414,6 @@ if __name__ == "__main__":
 					t.set_postfix(loss=testbed.loss)
 					old_training_step = testbed.training_step
 					tqdm_last_update = now
-
-	with open(args.evaluation_list, 'w', newline='') as file:
-		writer = csv.writer(file)
-		writer.writerows(stream_list)
-		writer.writerows([[time] for time in times_recorded])
-
 	if args.save_snapshot:
 		testbed.save_snapshot(args.save_snapshot, False)
 
